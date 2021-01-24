a7c32aae9fecef7be736aa35638fde2944951cb5
Tobias Ulrich <tobiasbulrich@gmail.com>
2021-01-24 18:39:12 -0300

    Update sandbox to test window implementation

--- sandbox/main.cpp
+++ sandbox/main.cpp
@@ -1,14 +1,25 @@
 
 #include <purpur/purpur.hpp>
+#include <purpur/platform/window.hpp>
+#include <purpur/platform/window_style.hpp>
 
 using namespace ppr;
 
 int main(int argc, char* argv[]) {
 
-
 	Logger log = Logger();
 
 	log.log("hello");
+
+	Window win = Window(480, 300, "teste", WindowStyle::Close | WindowStyle::Title | WindowStyle::Miniaturizable | WindowStyle::Resize);
+
+	win.setVisible(true);
+
+	while (win.isVisible()) {
+
+		win.process();
+
+	}
 
 	return 0;
 }
