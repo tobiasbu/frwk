33b685ccf539c201e800fb5eba6508ce86bd2c47
tobiasbu <flamenco.bluegrass@gmail.com>
2021-01-31 23:51:44 -0300

    Integrate Catch2

---
+++ tests/main.cpp
@@ -0,0 +1,28 @@
+#define CATCH_CONFIG_RUNNER
+#include <catch2/catch.hpp>
+
+int main(int argc, char* argv[])
+{
+	Catch::Session session; // There must be exactly one instance
+
+	 // Now pass the new composite back to Catch so it uses that
+
+	char* args[] = { argv[0], "-s" , "-r", "compact" };
+
+	int returnCode = session.applyCommandLine(4, args);
+	if (returnCode != 0) {// Indicates a command line error
+		printf("Failed to parse arguments. Exit code %d", returnCode);
+		return returnCode;
+	}
+
+	// writing to session.configData() or session.Config() here 
+	// overrides command line args
+	// only do this if you know you need to
+
+	int numFailed = session.run();
+
+	// numFailed is clamped to 255 as some unices only use the lower 8 bits.
+	// This clamping has already been applied, so just return it here
+	// You can also do any post run clean-up here
+	return numFailed;
+}
