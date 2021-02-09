000005910c72c93b23dd8b926e56538bc0ccb94f
tobiasbu <flamenco.bluegrass@gmail.com>
2021-02-09 00:45:58 -0300

    Adjust examples folder

---
+++ examples/simple_window/main.cpp
@@ -0,0 +1,33 @@
+
+#include <purpur/purpur.hpp>
+
+#include <purpur/platform/platform.hpp>
+#include <purpur/platform/window.hpp>
+
+using namespace ppr;
+
+int main(int argc, char* argv[]) {
+
+	Logger logger = Logger();
+
+	logger.log("purpurina frwk", PPR_FRWK_VERSION);
+
+
+	if (!Platform::init()) {
+		logger.log("Could not initialize Platform");
+		return 1;
+	}
+
+	logger.log("Opening window...");
+	Window win(480, 300, "teste");
+	win.setVisible(true);
+	while (win.isVisible()) {
+		win.process();
+	}
+
+	if (!Platform::terminate()) {
+		return 1;
+	}
+
+	return 0;
+}
