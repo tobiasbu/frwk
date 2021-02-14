578d9207b904f456c7c196a3841710c2a28a7cd6
tobiasbu <flamenco.bluegrass@gmail.com>
2021-02-14 14:32:12 -0300

    Create MOV and FWD macros
    Move and forward semantics implementation using macros.
    The reason to use them is to improve compilation time.
    
    See:
    https://www.foonathan.net/2020/09/move-forward/#self-documenting-code

--- include/purpur/core/utils/type_traits.hpp
+++ include/purpur/core/utils/type_traits.hpp
@@ -6,12 +6,14 @@
 
 namespace ppr {
 
+	// Remove reference
+
 	///
 	/// @brief Provides the non-reference type to which `T` refers.
 	///
 	template<class T>
 	struct remove_reference {
-		typedef T type; //!< type referred by T
+		using type = T; //!< type referred by T
 	};
 
 	///
@@ -19,7 +21,7 @@ namespace ppr {
 	///
 	template<class T>
 	struct remove_reference<T &> {
-		typedef T type; //!< type referred by T
+		using type = T; //!< type referred by T
 	};
 
 	///
@@ -27,24 +29,50 @@ namespace ppr {
 	///
 	template<class T>
 	struct remove_reference<T &&> {
-		typedef T type; //!< type referred by T
+		using type = T; //!< type referred by T
 	};
+
 
 	///
 	/// @brief Defines non reference type from T
 	///
-	///
 	template<class T>
 	using remove_reference_t = typename remove_reference<T>::type;
 
+
+
+	// move
+
 	////////////////////////////////////////////////////////////
-	/// @brief Returns an _r-value_ reference to argument.
+	/// @brief Returns an _rvalue_ reference to argument.
 	///
 	////////////////////////////////////////////////////////////
 	template<class T>
-	NODISCARD CONSTEXPR remove_reference_t<T> && move(T && t) NOEXCEPT {
-		return static_cast<remove_reference_t<T> &&>(t);
+	NODISCARD CONSTEXPR remove_reference_t<T> && move(T && arg) NOEXCEPT {
+		return static_cast<remove_reference_t<T> &&>(arg);
 	}
+
+	////////////////////////////////////////////////////////////
+	/// @brief `static_cast` to `rvalue` reference.
+	///
+	/// This macro is an version of move semantics.
+	/// The reason to use this macro is that improves compilation time.
+	///
+	/// @see https://www.foonathan.net/2020/09/move-forward/#self-documenting-code
+	///
+	////////////////////////////////////////////////////////////
+	#define MOV(...) \
+		 static_cast<ppr::remove_reference_t<decltype(__VA_ARGS__)>&&>(__VA_ARGS__)
+
+
+    ////////////////////////////////////////////////////////////
+    /// @brief Forward an _lvalue_ as either an _lvalue_ or an rvalue
+    ///
+    ////////////////////////////////////////////////////////////
+	#define FWD(...) \
+		static_cast<decltype(__VA_ARGS__)&&>(__VA_ARGS__)
+
+
 
 } // namespace ppr
 
--- tests/core/utils/type_traits.cpp
+++ tests/core/utils/type_traits.cpp
@@ -1,3 +1,4 @@
+
 #include <catch2/catch.hpp>
 
 #include <purpur/core/utils/type_traits.hpp>
@@ -10,6 +11,14 @@ namespace {
 		b = ppr::move(tmp);
 	}
 
+	template <class T>
+	void swapMacro(T& a, T& b) {
+		T tmp(MOV(a));
+		a = MOV(b);
+		b = MOV(tmp);
+	}
+
+
 	class DummyMoveClass {
 		public:
 		int value = 42;
@@ -17,9 +26,9 @@ namespace {
 	};
 }
 
-TEST_CASE( "move", "[core:utils:move.hpp]" ) {
+TEST_CASE( "move", "[core:utils:type_traits:move]" ) {
 
-	SECTION( "swap entities" ) {
+	SECTION("swap entities") {
 		DummyMoveClass a(64);
     	DummyMoveClass b(128);
 
@@ -29,6 +38,21 @@ TEST_CASE( "move", "[core:utils:move.hpp]" ) {
 		REQUIRE(b.value == 64);
 
 		swap(a, b);
+
+		REQUIRE(a.value == 64);
+		REQUIRE(b.value == 128);
+    }
+
+	SECTION("swap entities with MOV macro") {
+		DummyMoveClass a(64);
+    	DummyMoveClass b(128);
+
+		swapMacro(a, b);
+
+		REQUIRE(a.value == 128);
+		REQUIRE(b.value == 64);
+
+		swapMacro(a, b);
 
 		REQUIRE(a.value == 64);
 		REQUIRE(b.value == 128);
