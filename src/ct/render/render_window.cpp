367226dd6dad391cd5f8b249f2dfb4969b27d194
tobiasbu <flamenco.bluegrass@gmail.com>
2021-02-20 23:08:37 -0300

    Move internal platform headers to source folder

--- src/ct/render/render_window.cpp
+++ src/ct/render/render_window.cpp
@@ -5,8 +5,22 @@ namespace ct {
 
 	RenderWidow::RenderWidow(WindowImpl * nativeWindow) : Window(nativeWindow) {}
 
-	RenderWidow * RenderWidow::create() {
 
+	RenderWindowPtr create_render_window(uint32 width, uint32 height, cstr title, uint32 style, const ContextConfig & config) {
+
+		WindowProperties props(
+			width,
+			height,
+			title,
+			style
+		);
+
+		return create_render_window(props, config);
+	}
+
+	RenderWindowPtr create_render_window(const WindowProperties & properties, const ContextConfig & config) {
+		// auto impl = WindowImplType::create(props);
+		// return std::make_unique<enable_make<RenderWidow>>(impl);
 		return nullptr;
 	}
 
