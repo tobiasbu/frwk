79259b45182cba776e7f7ba692324dcaad20d240
Tobias Ulrich <tobiasbulrich@gmail.com>
2021-08-02 23:23:15 -0300

    Format CMake helpers

--- cmake/helpers.cmake
+++ cmake/helpers.cmake
@@ -1,10 +1,15 @@
 
 include(CMakeParseArguments)
 
+# ----------------------------------------------------------------------------------------------
+#
+# Get project major version
+#
 macro(get_project_version ct_ver)
 	set(${ct_ver} "${${PROJECT_NAME}_VERSION_MAJOR}")
 endmacro()
 
+# ----------------------------------------------------------------------------------------------
 #
 # Set Xcode property
 #
@@ -16,14 +21,14 @@ macro (ct_set_xcode_prop target xcode_prop xcode_value)
     set_property (TARGET ${TARGET} PROPERTY XCODE_ATTRIBUTE_${xcode_prop} ${xcode_value})
 endmacro ()
 
+# ----------------------------------------------------------------------------------------------
 #
 # Add Chronotrix directory
 #
 # @param package_name {STRING}	cmake target name
-# @param
 #
 function(ct_add_lib_directory package_name)
-	message("optional arguments: ${ARGN}")
+	# message("optional arguments: ${ARGN}")
 
     set(INCPATH ${CT_INC}/${package_name})
     set(SRCPATH ${CT_SRC}/${package_name})
@@ -31,6 +36,7 @@ function(ct_add_lib_directory package_name)
     add_subdirectory(${package_name})
 endfunction()
 
+# ----------------------------------------------------------------------------------------------
 #
 # Add Chronotrix library
 #
@@ -43,7 +49,7 @@ macro(ct_add_library target)
 	# Parse arguments
 	cmake_parse_arguments(ARGS "" "" "SOURCES;DEPENDS" ${ARGN})
 	if (NOT "${ARGS_UNPARSED_ARGUMENTS}" STREQUAL "" AND NOT ${ARGS_UNPARSED_ARGUMENTS} STREQUAL "LINK")
-        message(FATAL_ERROR "Extra unparsed arguments when calling ct_add_library: ${ARGS_UNPARSED_ARGUMENTS}")
+        message(FATAL_ERROR "ct_add_library: Extra unparsed arguments: ${ARGS_UNPARSED_ARGUMENTS}")
     endif()
 
 	# Create library target
@@ -103,6 +109,7 @@ macro(ct_add_library target)
 
 endmacro()
 
+# ----------------------------------------------------------------------------------------------
 #
 # Add Chronotrix header-only library
 #
@@ -127,15 +134,15 @@ macro(ct_add_header_lib target HEADERS)
 	target_include_directories(${target} INTERFACE
 							   $<BUILD_INTERFACE:${CT_FRWK_DIR}/include>
 							   $<INSTALL_INTERFACE:include>)
-	# target_sources(${target} INTERFACE ${HEADERS})
 
 	if(ARGS_DEPENDS)
 		target_link_libraries(${target} PUBLIC ${ARGS_DEPENDS})
 	endif()
 endmacro()
 
+# ----------------------------------------------------------------------------------------------
 #
-# Add a new example executable target
+# Add a new executable target
 #
 # @param target {TARGET}			cmake target
 # @param SOURCES {STRING[]}		list of files
@@ -181,15 +188,16 @@ macro(ct_add_executable target)
 							  "${CMAKE_BINARY_DIR}/bin/chronotrix_core_d.dll"        # <--this is in-file
 							  $<TARGET_FILE_DIR:${target}>)                 			# <--this is out-file path
 		else()
-			message(AUTHOR_WARNING "CT_EXAMPLES_POSTCOMMAND is not set correctly.\ The '${target}' target will be placed in ${CMAKE_BINARY_DIR}")
+			message(AUTHOR_WARNING "ct_add_executable: CT_EXAMPLES_POSTCOMMAND is not set correctly.\ The '${target}' target will be placed in ${CMAKE_BINARY_DIR}")
 		endif()
 	endif()
 endmacro()
 
+# ----------------------------------------------------------------------------------------------
 #
-# Add a new Sandbox executable target
+# Add a new test case
 #
-# @param target {TARGET}			cmake target
+# @param target {TARGET}		cmake target
 # @param SOURCES {STRING[]}		list of files
 # @param DEPENDS? {(TARGET | STRING)[]}	optional - list of dependencies
 #
@@ -213,6 +221,105 @@ macro(ct_add_test target SOURCES)
 
 endmacro()
 
+
+# ----------------------------------------------------------------------------------------------
+#
+# Create an INTERFACE library target from an external dependecy.
+#
+# @param target {TARGET}		library target name
+# @param INCLUDE? {string[]}	optional - list of includes
+# @param LINK? {string[]}		optional - list of libraries to link
+#
+function(ct_add_external)
+    list(GET ARGN 0 target)
+    list(REMOVE_AT ARGN 0)
+
+	if (TARGET ${target})
+        message(FATAL_ERROR "ct_add_external: Target '${target}' is already defined")
+    endif()
+
+	cmake_parse_arguments(ARGS "" "" "INCLUDE;LINK" ${ARGN})
+    if (ARGS_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "ct_add_external: Could not parse arguments: ${ARGS_UNPARSED_ARGUMENTS}")
+    endif()
+
+	# Create lib
+ 	add_library(${target} INTERFACE)
+
+	if (ARGS_INCLUDE)
+		foreach(include_item IN LISTS ARGS_INCLUDE)
+			if (NOT include_item)
+				message(FATAL_ERROR "ct_add_external: No path given for include dir ${ARGS_INCLUDE}")
+			endif()
+			target_include_directories(${target} INTERFACE $<BUILD_INTERFACE:${include_item}>)
+		endforeach()
+	endif()
+
+
+	if (ARGS_LINK)
+		foreach(lib_item IN LISTS ARGS_LINK)
+			if (NOT lib_item)
+				message(FATAL_ERROR "ct_add_external: missing item ${ARGS_LINK}")
+			endif()
+			target_link_libraries(${target} INTERFACE $<BUILD_INTERFACE:${lib_item}>)
+		endforeach()
+	endif()
+
+
+	install(TARGETS ${target} EXPORT ChronotrixConfigExport)
+endfunction()
+
+# ----------------------------------------------------------------------------------------------
+#
+# Find package and create INTERFACE library
+#
+# @param target {TARGET}		library target name
+# @param INCLUDE? {string[]}	optional - list of includes
+# @param LINK? {string[]}		optional - list of libraries to link
+#
+function(ct_find_and_create_interface)
+	list(GET ARGN 0 target)
+    list(REMOVE_AT ARGN 0)
+
+	if (TARGET ${target})
+        message(FATAL_ERROR "ct_find_and_create_interface: Target '${target}' is already defined")
+    endif()
+
+	cmake_parse_arguments(ARGS "" "" "INCLUDE;LINK" ${ARGN})
+    if (ARGS_UNPARSED_ARGUMENTS)
+        message(FATAL_ERROR "ct_find_and_create_interface: Could not parse arguments: ${ARGS_UNPARSED_ARGUMENTS}")
+    endif()
+
+
+	# if (OS_IOS)
+    #     find_host_package(${target} REQUIRED)
+    # else()
+	find_package(${target} REQUIRED)
+    # endif()
+
+	set(include_list "")
+	if (ARGS_INCLUDE)
+		foreach(include_item IN LISTS ARGS_INCLUDE)
+			list(APPEND include_list "${${include_item}}")
+		endforeach()
+    endif()
+
+	set(link_list "")
+	if (ARGS_LINK)
+		foreach(lib_item IN LISTS ARGS_LINK)
+			SET(current "${${lib_item}}")
+			if (NOT current)
+				message(FATAL_ERROR "ct_find_and_create_interface: Library not found: ${lib_item}")
+			endif()
+			list(APPEND link_list ${current})
+		endforeach()
+    endif()
+
+	ct_add_external(${target} INCLUDE ${include_list} LINK ${link_list})
+endfunction()
+
+
+# ----------------------------------------------------------------------------------------------
 #
 # Export targets
 #
